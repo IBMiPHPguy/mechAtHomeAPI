0ca9bcb62efabea26ac00f2cb67c680dde57bed0
Robert Binetti <rbine0525@gmail.com>
2015-02-22 00:57:05 -0700

    Moved the Route filter from routes.php to filters.php

--- app/filters.php
+++ app/filters.php
@@ -88,3 +88,36 @@ Route::filter('csrf', function()
 		throw new Illuminate\Session\TokenMismatchException;
 	}
 });
+
+/*
+|--------------------------------------------------------------------------
+| API token Protected Route Filter
+|--------------------------------------------------------------------------
+|
+| The Auth.Token filter is responsible for protecting application routes
+| that require an api token from being accessed. It requires a header
+| that sends the API token.
+|
+*/
+
+Route::filter('auth.token', function($route, $request)
+{
+		// Get the token from the request header X-Auth-Token
+    $token = $request->header('X-Auth-Token');
+
+		// Find a user record with the token
+    $user =  User::where('api_token', '=', $token)->first();
+
+		// If the header is not set or we cant find a user with that token
+		// respond withe a 'Not Authenticated' 401 JSON error return.
+		if(!$token || !$user) {
+        $response = Response::json([
+            'success' => false,
+            'message' => 'Not authenticated',
+            'code' => 401],
+            401
+        );
+        $response->header('Content-Type', 'application/json');
+    return $response;
+    }
+});
