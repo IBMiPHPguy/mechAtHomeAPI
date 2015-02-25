188e60d11a61f0af39cbd6d30e8ad725f7c30cf7
Robert Binetti <rbine0525@gmail.com>
2015-02-24 19:15:01 -0700

    Migration for phones

---
+++ app/database/migrations/2015_02_25_020139_create_phones_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhonesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('phones', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('user_id')->unsigned();
+			$table->foreign('user_id')->references('id')
+				->on('users')
+				->onDelete('cascade')
+				->onUpdate('cascade');
+			$table->string('phone_type',10);
+			$table->string('short_name',15)->nullable();
+			$table->boolean('text_flag');
+			$table->boolean('primary_nbr');
+			$table->integer('phone_nbr')->unsigned();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('phones');
+	}
+
+}
